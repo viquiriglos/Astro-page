d553eedb77e97091044f32027e3a039926328a29
viquiriglos <67325401+viquiriglos@users.noreply.github.com>
2023-04-09 08:25:59 +0000

    add submission form, connection to DB and acknowledgement reponse

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import load_courses_from_db, load_course_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_courses_from_db, load_course_from_db, add_application_to_db
 
 
 app=Flask(__name__)
@@ -21,6 +21,16 @@ def show_course(id):
   if not course_db:
     return "Not Found", 404
   return render_template("coursepage.html", course = course_db)
+
+@app.route("/courses/<id>/apply", methods=['post'])
+def apply_to_course(id):
+  data = request.form
+  course_db = load_course_from_db(id)
+  add_application_to_db(id, data)
+  return render_template('application_submitted.html',
+                         application = data,
+                         course = course_db)
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -27,3 +27,16 @@ def load_course_from_db(id):
       return None
     else:
       return rows[0]._asdict()
+
+def add_application_to_db(course_id, data):
+  with engine.connect() as conn:
+    query = text("INSERT INTO applications(course_id, first_names, last_names, email, birth_date, birth_place, observations) VALUES (:course_id, :first_names, :last_names, :email, :birth_date, :birth_place, :observations)")
+    conn.execute(query,
+                 {"course_id" : course_id,
+                 "first_names" : data['first_names'],
+                 "last_names" : data['last_names'],
+                 "email" : data['email'],
+                 "birth_date" : data['birth_date'],
+                 "birth_place" : data['birth_place'],
+                 "observations" : data['observations']}
+                )
